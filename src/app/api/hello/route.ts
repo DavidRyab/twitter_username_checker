6bd31c5345d37aa948f5faeeb993d05db6c2a535
Desmondezo1 <desezo@gmail.com>
2023-05-01 15:11:00 +0100

    fix build error

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,7 +1,4 @@
 import { NextRequest, NextResponse } from "next/server";
-import puppeteer from 'puppeteer-extra';
-const StealthPlugin = require('puppeteer-extra-plugin-stealth')
-
 
 export async function GET(request: NextRequest, response: NextResponse) {
 
