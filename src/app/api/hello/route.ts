c48f34663e334430bd431913931db7af0b9bb31b
Desmondezo1 <desezo@gmail.com>
2023-05-12 15:41:31 +0100

    using nextjs script

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import UserAgent from 'user-agents';
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -9,17 +10,68 @@ export async function GET(request: NextRequest) {
     if(!username) {
         throw new Error('username must be provided');
     }
-    const baseUrl = process.env.BACKEND_BASE_URL;
-    console.log(`base: ${baseUrl}. username: ${username}`)
-    const result  = axios.post(`${baseUrl}/check-username`, {username})
-    .then( (res: any) => res.data );
-    const resp = await result;
-    console.log({resp})
-    return NextResponse.json(resp);
+    const res = await checkName(username);
+    return NextResponse.json(res);
     
     } catch (error: any) {
        console.log(error.message);
     }
 
 }
+
+async function checkName(username: string){
+    try {
+      const agent = new UserAgent();
+      // const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36';
+      const userAgent = agent.toString();
+  
+      // Create an instance of axios with default configuration
+      const instance = axios.create();
+  
+      // Add a request interceptor to modify the configuration before sending the request
+      instance.interceptors.request.use(config => {
+      // Add a random delay between 1 and 5 seconds
+      const delay = Math.floor(Math.random() * 4000) + 1000;
+      return new Promise(resolve => setTimeout(() => resolve(config), delay));
+      });
+  
+      // Add a request interceptor to modify the user agent header
+      instance.interceptors.request.use(config => {
+      config.headers['User-Agent'] = userAgent;
+      return config;
+      });
+  
+  
+    const response = await axios.get(`https://proxy.scrapeops.io/v1/?api_key=6bcf45ae-d3c3-4bdb-b1b9-5f9e93021431&url=https://www.instagram.com/${username}`
+      );
+      const body = response.data;
+      
+      const count = (body.match(new RegExp(username, 'gi')) || []).length;
+      console.log(`Found ${count} occurrences of "${username}"`);
+      if(count > 30){
+           return {
+          data: {
+            result: false
+          },
+          error: null
+        } 
+      } else {
+        return {
+          data: {
+            result: true
+          },
+          error: null
+        } 
+      }
+  
+    } catch (error: any) {
+      console.error(`Error fetching ${username}: ${error.message}`);
+      return {
+        data: null,
+        error: {
+          reason: error.message
+        }
+      }
+    }
+  }
   
