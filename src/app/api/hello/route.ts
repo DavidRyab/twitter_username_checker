9277bf8f2b6e0b3fe2eee33840bbc2495dda0e8f
Desmondezo1 <desezo@gmail.com>
2023-05-28 13:59:26 +0100

    updated twitter checker

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import UserAgent from 'user-agents';
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -21,48 +20,10 @@ export async function GET(request: NextRequest) {
 
 async function checkName(username: string){
     try {
-      const agent = new UserAgent();
-      // const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36';
-      const userAgent = agent.toString();
-  
-      // Create an instance of axios with default configuration
-      const instance = axios.create();
-  
-      // Add a request interceptor to modify the configuration before sending the request
-      instance.interceptors.request.use(config => {
-      // Add a random delay between 1 and 5 seconds
-      const delay = Math.floor(Math.random() * 4000) + 1000;
-      return new Promise(resolve => setTimeout(() => resolve(config), delay));
-      });
-  
-      // Add a request interceptor to modify the user agent header
-      instance.interceptors.request.use(config => {
-      config.headers['User-Agent'] = userAgent;
-      return config;
-      });
-  
-  
-    const response = await axios.get(`https://proxy.scrapeops.io/v1/?api_key=6bcf45ae-d3c3-4bdb-b1b9-5f9e93021431&url=https://www.instagram.com/${username}`
-      );
-      const body = response.data;
-      
-      const count = (body.match(new RegExp(username, 'gi')) || []).length;
-      console.log(`Found ${count} occurrences of "${username}"`);
-      if(count > 30){
-           return {
-          data: {
-            result: false
-          },
-          error: null
-        } 
-      } else {
-        return {
-          data: {
-            result: true
-          },
-          error: null
-        } 
-      }
+    const response = await axios.post(`${process.env.BACKEND_BASE_URL}/check-username`,{username});
+    const body = response.data;
+    console.log({body})
+    return body
   
     } catch (error: any) {
       console.error(`Error fetching ${username}: ${error.message}`);
